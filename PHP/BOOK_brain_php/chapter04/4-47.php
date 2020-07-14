0d754d295252cd5c87ff2284f9ef999e7184e405
archer051 <archer@kakao.com>
2020-07-14 10:48:02 +0900

    20200715 Ver0.2 커밋

--- PHP/BOOK_brain_php/chapter04/4-47.php
+++ PHP/BOOK_brain_php/chapter04/4-47.php
@@ -2,5 +2,5 @@
 	$christmas = "2007-12-25";
 
 	$date = sscanf($christmas, "%d-%d-%d", &$year, &$month, &$day);
-	echo "{$year}³â {$month}¿ù {$day}ÀÏÀº Å©¸®½º¸¶½ºÀÔ´Ï´Ù.";
+	echo "{$year}ë…„ {$month}ì›” {$day}ì¼ì€ í¬ë¦¬ìŠ¤ë§ˆìŠ¤ìž…ë‹ˆë‹¤.";
 ?>
